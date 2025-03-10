ab016071c7f1b43cc2aaf4a603cc1b9a149cb38f
jpsileskh <jpsileskh@hotmail.com>
2025-03-10 09:20:12 -0300

    Regras de tipagem e input

---
+++ aula03/Tiposvariaveis.java
@@ -0,0 +1,14 @@
+public class Tiposvariaveis {
+  public static void main(String[] args) {
+    int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+    System.out.println("esse é meu número" + myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
